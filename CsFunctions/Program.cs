ccac1dee50c5e503ce8c552461966d03eb808145
colombovf <colombovf@gmail.com>
2022-11-24 14:38:55 +0100

    Aggiungere i file di progetto.

---
+++ CsFunctions/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*void StampaArrayInteri(int[] array): che preso un array di numeri interi, stampa a video il contenuto
+dell’array in questa forma “[elemento 1, elemento 2, elemento 3, ...]”.*/
+
+int[] arrayInteri = {1,2,3,4,5,6,7,8,9,10};
+StampaArrayInteri(arrayInteri);
+void StampaArrayInteri(int[] arrayInteri)
+{
+    Console.Write("[");
+    for (int i = 0; i < arrayInteri.Length; i++)
+    {
+        if (i == arrayInteri.Length - 1)
+        {
+            Console.Write('"' + arrayInteri[i]);
+        }
+        else
+        {
+            Console.Write('"' + arrayInteri[i]+ " , ");
+        }
+    }
+    Console.WriteLine("]");
+    /* VERSIONE FOREACH 
+     
+     * foreach(int numero in arrayInteri){
+     *      Console.WriteLine(numero);ù
+     * }
+    
+     */
+}
