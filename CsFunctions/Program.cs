2b8b5384add688fe9611593cb9a8c7530b552cd5
colombovf <colombovf@gmail.com>
2022-11-24 15:04:38 +0100

    Parte 3

--- CsFunctions/Program.cs
+++ CsFunctions/Program.cs
@@ -2,6 +2,8 @@
 /*void StampaArrayInteri(int[] array): che preso un array di numeri interi, stampa a video il contenuto
 dell’array in questa forma “[elemento 1, elemento 2, elemento 3, ...]”.*/
 
+using System;
+
 int[] arrayInteri = {1,2,3,4,5,6,7,8,9,10};
 StampaArrayInteri(arrayInteri);
 void StampaArrayInteri(int[] arrayInteri)
@@ -25,9 +27,29 @@ void StampaArrayInteri(int[] arrayInteri)
  * quadrato del numero passato come parametro.*/
 int number = 4;
 QuadratoDelNumero(number);
-
 void QuadratoDelNumero (int number)
 {
     number*= number;
     Console.WriteLine(number);
+}
+
+/*Preso un array di numeri interi, restituisca un nuovo array 
+ * con tutti gli elementi elevati quadrato.Attenzione: è importante
+ * restituire un nuovo array, e non modificare l’array come parametro della funzione! */
+
+int[] integerArray = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+int[] copiaArray = (int[])integerArray.Clone();
+ElevaArratAlQuadrato(copiaArray);
+
+for (int i = 0; i< integerArray.Length; i++)
+{
+    Console.Write(integerArray[i]);
+}
+void ElevaArratAlQuadrato(int[] array)
+{
+    for (int i = 0; i<array.Length; i++)
+    {
+        array[i] *= array[i];
+        Console.WriteLine(array[i]);
+    }
 }
